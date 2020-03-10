0fa7db2f5d50c7303e81f4dbd4d9eb93828bbfa8
Anuhya98 <59390963+Anuhya98@users.noreply.github.com>
2020-03-10 12:17:23 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+public class Add{
+public static void main(String[] args){
+int a;
+System.out.println("");
+int a=Interger.parseInt(args[0]);
+if(a%2==0)
+System.out.println("Even");
+else
+System.out.println("Odd");
+}
+}
+
