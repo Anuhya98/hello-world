274d9c158991da6ffe42624b99ab1423366769fe
Anuhya98 <59390963+Anuhya98@users.noreply.github.com>
2020-03-10 11:56:39 +0530

    Add files via upload

---
+++ TodayDate.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+public class TodayDate{
+public static void main(String[] args){
+LocalDate date=LocalDate.now();
+System.out.println("Todays date is :"+date);
+
+}
+}
+
